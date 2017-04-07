78784a1e4664a98f699ddcd9b3ddd9d9d8ae2979
Anessa Ortner <anessa.ortner@gmail.com>
2017-04-06 20:22:28 -0500

    beginning switching to jQuery

--- songs.js
+++ songs.js
@@ -1,43 +1,42 @@
 //VARIABLES
 var songs = [];
-var artist = "";
-var album = "";
-var songName = "";
-var songDiv = document.getElementById("songList")
+// var songDiv = document.getElementById("songList")
 
-/********************************
-INITIAL PRINT TO DOM
-********************************/
+// //********************************
+// //INITIAL PRINT TO DOM
+// //********************************
 
-function songsToDOM(array) {
-	var songListString = "";
-	for (let x = 0; x < array.length; x++) {
-		var arrayLength = x + array.length;
-		array[x].id = arrayLength;
-		songListString += `<section id="${arrayLength}"><h1 class="title" id="title">${array[x].song}</h1>`;
-		songListString += `<span class="artist-name" id="artist-name">${array[x].artist}</span> |`
-		songListString += `<span class="album-name" id="album-name">${array[x].album}<button id="deleteBtn" class="deleteBtn">DELETE</button></span></section>`
-	}
-	songDiv.innerHTML = songListString;
+// function songsToDOM(array) {
+// 	var songListString = "";
+// 	for (let x = 0; x < array.length; x++) {
+// 		var arrayLength = x + array.length;
+// 		array[x].id = arrayLength;
+
+
+// 		songListString += `<section id="${arrayLength}"><h1 class="title" id="title">${array[x].song}</h1>`;
+// 		songListString += `<span class="artist-name" id="artist-name">${array[x].artist}</span> |`
+// 		songListString += `<span class="album-name" id="album-name">${array[x].album}<button id="deleteBtn" class="deleteBtn">DELETE</button></span></section>`
+// 	}
+// 	songDiv.innerHTML = songListString;
 	
-	var moreBtn = document.getElementById("moreBtn");
-	var deleteBtn = document.getElementsByClassName("deleteBtn");
-	moreBtn.addEventListener("click", moreClick)
-	for (let i = 0; i < deleteBtn.length; i++){
-		deleteBtn[i].addEventListener("click", deleteDiv);
-	}
+// 	var moreBtn = document.getElementById("moreBtn");
+// 	var deleteBtn = document.getElementsByClassName("deleteBtn");
+// 	moreBtn.addEventListener("click", moreClick)
+// 	for (let i = 0; i < deleteBtn.length; i++){
+// 		deleteBtn[i].addEventListener("click", deleteDiv);
+// 	}
 
-};
+// };
 
-function buildSongData(data){
-  	for (let y = 0; y < data.songs.length; y++) {
-  		songs.push(data.songs[y]);
-  	}
-}
+// function buildSongData(data){
+//   	for (let y = 0; y < data.songs.length; y++) {
+//   		songs.push(data.songs[y]);
+//   	}
+// }
 
-/********************************
-XHR PRODUCTS FUNCTION EXECUTIONS
-********************************/
+//********************************
+//XHR PRODUCTS FUNCTION EXECUTIONS
+//********************************
 
 function songsLoad(){
 	var data = JSON.parse(this.responseText);
@@ -46,12 +45,12 @@ function songsLoad(){
 };
 
 function loadFail(){
-	songDiv.innerHTML = "Oops... Song information isn't loading!"
+	$("#songList").html("Oops... Song information isn't loading!");
 };
 
-/********************************
-XMLH REQUEST FOR SONG LIST
-********************************/
+//********************************
+//XMLH REQUEST FOR SONG LIST
+//********************************
 
 var myRequest = new XMLHttpRequest();
 myRequest.addEventListener("load", songsLoad);
@@ -68,23 +67,19 @@ function moreClick () {
 	myRequest2.open("GET", "songList2.json");
 	myRequest2.send();
 
-	var clickedBtn = document.getElementById("moreBtn");
-	clickedBtn.disabled = true;
-};
+// 	var clickedBtn = document.getElementById("moreBtn");
+// 	clickedBtn.disabled = true;
+// };
 
-function deleteDiv (e) {
-	var arrayId = e.target.parentNode.parentNode.id;
-	for (let x = 0; x < songs.length; x++) {
-		if (parseInt(arrayId) === songs[x].id) {
-			songs.splice(x, 1);
-		}
-	}
-	songsToDOM(songs);
-}
-
-
-
-
+// function deleteDiv (e) {
+// 	var arrayId = e.target.parentNode.parentNode.id;
+// 	for (let x = 0; x < songs.length; x++) {
+// 		if (parseInt(arrayId) === songs[x].id) {
+// 			songs.splice(x, 1);
+// 		}
+// 	}
+// 	songsToDOM(songs);
+// }
 
 
 
